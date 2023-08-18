c45ec7dba1f46ddbebc3ac4e211cba3dccbad9e9
johann Arley Cruz Méndez <90436414+cMjo21@users.noreply.github.com>
2023-08-17 19:28:13 -0500

    wrappers trabajo bloque1

--- src/main/java/B1/TrabajoBloque1.java
+++ src/main/java/B1/TrabajoBloque1.java
@@ -1,4 +1,58 @@
 package B1;
 
 public class TrabajoBloque1 {
+
+    public static void main(String[] args) {
+
+        // Boolean
+        boolean b = true;
+        Boolean bObject = new Boolean(b);
+        System.out.println(b);
+        System.out.println(bObject);
+
+        // Byte
+        byte c = 10;
+        Byte cObject = new Byte(c);
+        System.out.println(b); // 10
+        System.out.println(cObject); // 10
+
+        // Character
+        char d = 'a';
+        Character dObject = new Character(d);
+        System.out.println(d);
+        System.out.println(dObject);
+
+        // Short
+        short s = 100;
+        Short sObject = new Short(s);
+        System.out.println(s);
+        System.out.println(sObject);
+
+        // Integer
+        int i = 1000;
+        Integer iObject = new Integer(i);
+        System.out.println(i);
+        System.out.println(iObject);
+
+        // Long
+        long l = 1000000;
+        Long lObject = new Long(l);
+        System.out.println(l);
+        System.out.println(lObject);
+
+        // Float
+        float f = 1.2f;
+        Float fObject = new Float(f);
+        System.out.println(f); // 1.2
+        System.out.println(fObject);
+
+        // Double
+        double h = 1.23456789;
+        Double hObject = new Double(h);
+        System.out.println(d);
+        System.out.println(hObject);
+
+
+
+    }
 }
